76b29c1e6fc4bf51f0dca5558c2308e543051d84
Kerry Patrick <kpatrick@mmmicrosystems.com>
2023-08-26 08:34:10 -0500

    enums

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number,string]; // tuple
+// } = {
+//     name: 'Kerry',
+//     age: 48,
+//     hobbies: ['knitting', 'running'],
+//     role: [17, 'developer']
+// };
+// person.role.push('admin');  //warning: still supported
+// console.log(person.role);
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
 var person = {
     name: 'Kerry',
     age: 48,
     hobbies: ['knitting', 'running'],
-    role: [17, 'developer']
+    role: Role.AUTHOR
 };
-person.role.push('admin'); //warning: still supported
-console.log(person.role);
 var favoriteActivities;
 favoriteActivities = ['working out', 'writing code'];
 console.log(person.name);
@@ -13,4 +34,10 @@ for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
     // console.log(hobby.map()); // !!! ERROR !!!
+}
+if (person.role == Role.ADMIN) {
+    console.log('is admin');
+}
+else if (person.role == Role.AUTHOR) {
+    console.log('is author');
 }
--- app.ts
+++ app.ts
@@ -1,17 +1,30 @@
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number,string]; // tuple
-} = {
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number,string]; // tuple
+// } = {
+//     name: 'Kerry',
+//     age: 48,
+//     hobbies: ['knitting', 'running'],
+//     role: [17, 'developer']
+// };
+
+// person.role.push('admin');  //warning: still supported
+// console.log(person.role);
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role { ADMIN = 5, READ_ONLY = 'FRED', AUTHOR = 200 };
+
+const person = {
     name: 'Kerry',
     age: 48,
     hobbies: ['knitting', 'running'],
-    role: [17, 'developer']
+    role: Role.AUTHOR
 };
-
-person.role.push('admin');  //warning: still supported
-console.log(person.role);
 
 let favoriteActivities: string[];
 favoriteActivities = ['working out', 'writing code'];
@@ -21,4 +34,11 @@ console.log(person.name);
 for(const hobby of person.hobbies){
     console.log(hobby.toUpperCase());    
     // console.log(hobby.map()); // !!! ERROR !!!
+}
+
+if(person.role == Role.ADMIN){
+    console.log('is admin');
+}
+else if(person.role == Role.AUTHOR){
+    console.log('is author');
 }
