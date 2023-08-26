d11d0b09d60f29b00060dba717b9537901b4c8bc
Kerry Patrick <kpatrick@mmmicrosystems.com>
2023-08-26 08:19:47 -0500

    adding typed arrays

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-// const person: {
-//     name: string;
-//     age: number;
-// } = {
 var person = {
-  name: 'Kerry',
-  age: 48,
-  hobbies: ['knitting', 'cooking'],
+    name: 'Kerry',
+    age: 48,
+    hobbies: ['knitting', 'running']
 };
+var favoriteActivities;
+favoriteActivities = ['working out', 'writing code'];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()); // !!! ERROR !!!
+}
--- app.ts
+++ app.ts
@@ -1,10 +1,15 @@
-// const person: {
-//     name: string;
-//     age: number;
-// } = {
 const person = {
     name: 'Kerry',
-    age: 48
+    age: 48,
+    hobbies: ['knitting', 'running']
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ['working out', 'writing code'];
+
 console.log(person.name);
+
+for(const hobby of person.hobbies){
+    console.log(hobby.toUpperCase());    
+    // console.log(hobby.map()); // !!! ERROR !!!
+}
