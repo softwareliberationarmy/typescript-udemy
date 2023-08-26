a09ad9aeeec0b7185b43e530a64b8c7d5617994e
Kerry Patrick <kpatrick@mmmicrosystems.com>
2023-08-26 09:45:30 -0500

    callback functions

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ function add(n1, n2) {
 function printResult(num) {
     console.log('Result: ' + num);
 }
-console.log(printResult(add(5, 12)));
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+// console.log(printResult(add(5, 12)));
 var combineValues;
 combineValues = add;
 // combineValues = printResult;
 // combineValues = 5;
 console.log(combineValues(8, 8));
 // let someValue: undefined;
+addAndHandle(10, 20, printResult);
