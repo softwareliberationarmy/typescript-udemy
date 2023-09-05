3c631e4c2809c5eea66ca890e3ac487a054bb842
Kerry Patrick <kpatrick@mmmicrosystems.com>
2023-09-05 11:37:25 -0500

    build our own generics

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,14 @@
 "use strict";
-const names = [];
-const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('agdadsgd');
-    }, 2000);
-});
-promise.then((data) => {
-    data.split(' ');
-});
+// const names: Array<string> = [];
+// const promise = new Promise<string>((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('agdadsgd');
+//   }, 2000);
+// });
+// promise.then((data) => {
+//   data.split(' ');
+// });
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+const mergedObj = merge({ name: 'Fred' }, { age: 23 });
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
-const names: Array<string> = [];
+// const names: Array<string> = [];
 
-const promise = new Promise<string>((resolve, reject) => {
-  setTimeout(() => {
-    resolve('agdadsgd');
-  }, 2000);
-});
+// const promise = new Promise<string>((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('agdadsgd');
+//   }, 2000);
+// });
 
-promise.then((data) => {
-  data.split(' ');
-});
+// promise.then((data) => {
+//   data.split(' ');
+// });
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA!, objB);
+}
+
+const mergedObj = merge({ name: 'Fred' }, { age: 23 });
