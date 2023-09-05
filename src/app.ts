a31c890c5723f2461f724b9faa8b21eab65e1f0d
Kerry Patrick <kpatrick@mmmicrosystems.com>
2023-09-05 09:14:27 -0500

    intersection types as interfaces instead

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,26 @@
-type Admin = {
+interface Admin {
   name: string;
   privileges: string[];
-};
+}
 
-type Employee = {
+interface Employee {
   name: string;
   startDate: Date;
-};
+}
 
-type ElevatedEmployee = Admin & Employee;
+interface ElevatedEmployee extends Admin, Employee {}
+
+//   type Admin = {
+//   name: string;
+//   privileges: string[];
+// };
+
+// type Employee = {
+//   name: string;
+//   startDate: Date;
+// };
+
+// type ElevatedEmployee = Admin & Employee;
 
 const e1: ElevatedEmployee = {
   name: 'Max',
