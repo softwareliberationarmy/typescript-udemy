f0f838e62bbd7a853cb0d8927d2ffd37434807ae
Kerry Patrick <kpatrick@mmmicrosystems.com>
2023-09-05 11:40:34 -0500

    generic constraints

--- dist/app.js
+++ dist/app.js
@@ -11,4 +11,5 @@
 function merge(objA, objB) {
     return Object.assign(objA, objB);
 }
-const mergedObj = merge({ name: 'Fred' }, { age: 23 });
+const mergedObj = merge({ name: 'Fred', hobbies: ['knitting'] }, { age: 30 });
+console.log(mergedObj);
--- src/app.ts
+++ src/app.ts
@@ -10,8 +10,9 @@
 //   data.split(' ');
 // });
 
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA!, objB);
 }
 
-const mergedObj = merge({ name: 'Fred' }, { age: 23 });
+const mergedObj = merge({ name: 'Fred', hobbies: ['knitting'] }, { age: 30 });
+console.log(mergedObj);
