de49f7840b50aec993a9654d7d28d9ae17e8c1ff
Kerry Patrick <kpatrick@mmmicrosystems.com>
2023-09-05 11:47:33 -0500

    more on generic constraints

--- dist/app.js
+++ dist/app.js
@@ -13,3 +13,14 @@ function merge(objA, objB) {
 }
 const mergedObj = merge({ name: 'Fred', hobbies: ['knitting'] }, { age: 30 });
 console.log(mergedObj);
+function countAndDescribe(element) {
+    let descriptionText = 'Got no value';
+    if (element.length === 1) {
+        descriptionText = `Got 1 element.`;
+    }
+    if (element.length > 1) {
+        descriptionText = `Got ${element.length} elements.`;
+    }
+    return [element, descriptionText];
+}
+console.log(countAndDescribe(['Hi, there', 'Hello', 'Salut']));
--- src/app.ts
+++ src/app.ts
@@ -16,3 +16,20 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 
 const mergedObj = merge({ name: 'Fred', hobbies: ['knitting'] }, { age: 30 });
 console.log(mergedObj);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = 'Got no value';
+  if (element.length === 1) {
+    descriptionText = `Got 1 element.`;
+  }
+  if (element.length > 1) {
+    descriptionText = `Got ${element.length} elements.`;
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe(['Hi, there', 'Hello', 'Salut']));
